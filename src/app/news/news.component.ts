128d80afed012856364f6146dd9f37c6c0ca70d9
unknown <hagigat.sajjad.3@gmail.com>
2019-06-18 17:02:27 +0430

    front-end-v1.1

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StyleService } from '../Services/style.service';
 
 @Component({
   selector: 'app-news',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public styleservice:StyleService) { }
 
   ngOnInit() {
   }
